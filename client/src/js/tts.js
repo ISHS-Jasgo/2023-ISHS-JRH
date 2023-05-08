4d84fdc2d993bb4563bd43a85d5ffb9eed45213e
0917ba <0917ba@naver.com>
2023-05-08 22:01:46 +0900

    auth check

--- client/src/js/tts.js
+++ client/src/js/tts.js
@@ -1,3 +1,26 @@
+import textToSpeech from "@google-cloud/text-to-speech";
+
+const client = new textToSpeech.TextToSpeechClient({
+  keyFilename: "./../../../api/celestial-shore-380106-8271a95fb3ec.json",
+});
+
+console.log(client);
+
+// async function quickStart(text) {
+//   const request = {
+//     input: { text: text },
+//     // Select the language and SSML voice gender (optional)
+//     voice: { languageCode: "ko-KR", ssmlGender: "NEUTRAL" },
+//     // select the type of audio encoding
+//     audioConfig: { audioEncoding: "LINEAR16", speakingRate: 1 },
+//   };
+
+//   const [response] = await client.synthesizeSpeech(request);
+//   console.log("$$$ : ", response);
+
+//   return response.audioContent;
+// }
+
 function getSpeech(text) {
   const lang = "ko-KR";
   const utterThis = new SpeechSynthesisUtterance(text);
