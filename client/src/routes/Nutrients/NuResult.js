890874b59913e83aee32c84769901c9abd271412
Jasgo <70329768+ISHS-Jasgo@users.noreply.github.com>
2023-05-15 22:48:23 +0900

    import mistake haha

--- client/src/routes/Nutrients/NuResult.js
+++ client/src/routes/Nutrients/NuResult.js
@@ -1,6 +1,7 @@
 import { useLocation } from "react-router-dom";
 import { textToSpeech } from "./../../js/tts";
 import { useEffect } from "react";
+import { readJson } from "../../js/read";
 
 function NuResult() {
   const location = useLocation();
